6265fc7fc71ad67aaf2ecd6d7f027debc99013f9
Test <Hammertyme101@gmail.com>
2023-12-29 14:18:11 -0500

    dotenv

--- index.js
+++ index.js
@@ -8,7 +8,15 @@ import dalleRoutes from './routes/dalleRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+
+//app.use(cors());
+app.use(
+  cors({
+    origin: ['http://localhost:5173']  // <== URL of our future React app
+  })
+);
+
+
 app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/v1/post', postRoutes);
--- routes/dalleRoutes.js
+++ routes/dalleRoutes.js
@@ -28,6 +28,7 @@ router.route('/').post( async (req, res) => {
     });
 
     const image = aiResponse.data.data[0].b64_json;
+    console.log("AI Response", aiResponse)
     res.status(200).json({ photo: image });
   } catch (error) {
     console.error(error);
